2762abab9fef3cc4f6d0797e2c1931b0cfcea66a
CaesarChurch4 <21332832@student.uwl.ac.uk>
2016-11-02 18:28:54 +0000

    Add files via upload

---
+++ CompileTimeFixDemo1Version1.py
@@ -0,0 +1,8 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> x=int(2)
+>>> print("x squared is",x**2)
+x squared is 4
+>>> xcubed=print(x**3)
+8
+>>> 
---
+++ CompileTimeFixDemo1Version2.py
@@ -0,0 +1,9 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> x=2
+>>> print("x squared is",x**2)
+x squared is 4
+>>> xcubed=x**3
+>>> print(xcubed)
+8
+>>> 
