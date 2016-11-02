30244d85d1005a1bbab2b4463a96b5d117849d27
CaesarChurch4 <21332832@student.uwl.ac.uk>
2016-11-02 18:09:43 +0000

    Add files via upload
    Demonstrates 2 decimal places

---
+++ DecimalPointDemo1Run.py
@@ -0,0 +1,12 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> x=19.93
+>>> y=20.00
+>>> z=y-x
+>>> print(z)
+0.07000000000000028
+>>> print("%.2f"%z)
+0.07
+>>> print("{:.2f}".format(z))
+0.07
+>>> 
