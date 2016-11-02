1b425a379f89bc5b32684f2b69281c1270fe4b8d
CaesarChurch4 <21332832@student.uwl.ac.uk>
2016-11-02 17:42:50 +0000

    Add files via upload

---
+++ MultiplicationDemoRun1.py
@@ -0,0 +1,8 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> x=4
+>>> y=5
+>>> a=3*(x+y)
+>>> print(a)
+27
+>>> 
