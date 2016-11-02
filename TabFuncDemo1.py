eb604a5a23829d2d37a9d5b343681c79aed897b5
CaesarChurch4 <21332832@student.uwl.ac.uk>
2016-11-02 18:56:21 +0000

    Add files via upload

---
+++ TabFuncDemo1.py
@@ -0,0 +1,15 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print("Hello\t, next year you will be\t\t years old!")
+Hello	, next year you will be		 years old!
+>>> print("Hello\t, next year you will be\tyears old!")
+Hello	, next year you will be	years old!
+>>> print("Hello\t, next year you will be\t years old!")
+Hello	, next year you will be	 years old!
+>>> print("Hello\t, next year you will be \t years old!")
+Hello	, next year you will be 	 years old!
+>>> print("Hello \t, next year you will be \t years old!")
+Hello 	, next year you will be 	 years old!
+>>> print("Hello \t , next year you will be \t years old!")
+Hello 	 , next year you will be 	 years old!
+>>> 
