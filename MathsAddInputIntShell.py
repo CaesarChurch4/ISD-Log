85aaa813fc3b044fde6c96174f89517f71c16878
CaesarChurch4 <21332832@student.uwl.ac.uk>
2016-11-02 16:05:06 +0000

    Add files via upload

---
+++ MathsAddInputIntShell.py
@@ -0,0 +1,9 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> num1 = int(input("Enter first number: "))
+Enter first number: 6
+>>> num2 = int(input("Enter second number: "))
+Enter second number: 8
+>>> print("Addition using + is","\t", num1+num2)
+Addition using + is 	 14
+>>> 
