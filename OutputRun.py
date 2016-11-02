65087bee75a3186ddef2464ef4633abd80337642
CaesarChurch4 <21332832@student.uwl.ac.uk>
2016-11-02 15:24:08 +0000

    OutputRun

---
+++ OutputRun.py
@@ -0,0 +1,10 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: E:\UWL\1. Software Development\b. Module Content\Practicals\Python Codes\OutputShell.py 
+Hi there, what is your name?
+Please enter your name: Julius
+Hello
+Julius
+How are you?
+>>> 
