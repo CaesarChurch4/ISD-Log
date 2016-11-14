30b3a1fd388d8dcbdc1e58393cc75e58371e1202
CaesarChurch4 <21332832@student.uwl.ac.uk>
2016-11-14 16:39:00 +0000

    Delete P4Q1Run to 2dp.py

--- P4Q1Run to 2dp.py
+++
@@ -1,7 +0,0 @@
-Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> 
- RESTART: E:\UWL\1. Software Development\b. Module Content\Practicals\Python Codes\P4Q1Shell to 2dp.py 
-Radius: 5
-78.54
->>> 
